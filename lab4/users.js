fad50365ac23a863e245745b87c28daec2ae4e2b
Theanshukr <imanshu2702@gmail.com>
2026-09-17 11:03:26 +0530

    user

---
+++ lab4/users.js
@@ -0,0 +1,9 @@
+// we use in memory database
+let users = [
+    {id:1,name:'Amit Sharma',mob:'98345xxxxx',email:'amit.example@exam.com'},
+    {id:2,name:'Monika Verma',mob:'92345xxxxx',email:'moni.example@exam.com'},
+]
+
+let nextId = 3;
+
+export const getUsers = () => users;
